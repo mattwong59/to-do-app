63f9c494e6070670957209ee211dd09166235a46
Matt Wong <mattwong59@gmail.com>
2018-06-18 16:21:18 -0700

    Add radio buttons to dropdown

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,17 @@ import './App.css';
 
 class App extends Component {
   state = {
-    goals: ""
+    goals: "",
+    type: {
+      daily: false,
+      core: false,
+      shortTerm: false,
+      longTerm: false
+    },
+    colDaily: [],
+    colCore: [],
+    colShortTerm: [],
+    colLongTerm: []
   }
 
   componentDidMount() {
@@ -16,9 +26,18 @@ class App extends Component {
   render() {
     function handleClick(e) {
       e.preventDefault();
-      console.log(this.state.goals);
       this.state.goals.classList.toggle('open');
     }
+
+    function handleType(e) {
+      e.preventDefault();
+      console.log(this.state.type)
+      const type = {...this.state.type};
+      console.log('type', type)
+      type.daily = true;
+      this.setState({type})
+    }
+
     return (
       <div className="App">
           <h1>To Do App</h1>
@@ -26,15 +45,28 @@ class App extends Component {
             <label htmlFor="goal">New Goal</label>
             <input id="goal" type="text" name="goal" />
             <div onClick={handleClick.bind(this)} className="dropdown">
-              <button className="dropbtn">Dropdown</button>            
+              <button className="dropbtn">Priority</button>            
               <div className="dropdown-content">
-                <a>Daily</a>
+                <input className="choice" type="radio" id="goalChoice1"
+                name="contact" value="daily"/>
+                <label htmlFor="goalChoice1">Daily</label>
+                <input className="choice" type="radio" id="goalChoice2"
+                name="contact" value="core"/>
+                <label htmlFor="goalChoice2">Core</label>
+                <input className="choice" type="radio" id="goalChoice3"
+                name="contact" value="shortTerm"/>
+                <label htmlFor="goalChoice3">Short Term</label>
+                <input className="choice" type="radio" id="goalChoice4"
+                name="contact" value="longTerm"/>
+                <label htmlFor="goalChoice4">Long Term</label>
+                {/* <a onClick={handleType.bind(this)}>Daily</a>
                 <a>Core</a>
                 <a>Short-term</a>
-                <a>Long-term</a>
+                <a>Long-term</a> */}
               </div>
             </div>
           </form>
+
           <div className="columns">
             <div>
               <h2>Daily</h2>
