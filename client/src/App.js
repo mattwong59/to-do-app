52bf80c6f2341302cbe773e7dfeadef24bf2c764
Matt Wong <mattwong59@gmail.com>
2018-06-18 13:07:20 -0700

    Change header on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <h3>Matt</h3>
+          <h3>To Do App</h3>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
