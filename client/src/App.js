68fa68396c2cf7f7a08ef27a40656daabb8327bf
Matt Wong <mattwong59@gmail.com>
2018-06-18 15:12:13 -0700

    Add dropdown for goal types to form

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+  state = {
+    goals: ""
+  }
+
+  componentDidMount() {
+    const goalTypes = document.getElementsByClassName('dropdown-content');
+    this.setState({goals: goalTypes[0]});
+  }
+
   render() {
+    function handleClick(e) {
+      e.preventDefault();
+      console.log(this.state.goals);
+      this.state.goals.classList.toggle('open');
+    }
     return (
       <div className="App">
           <h1>To Do App</h1>
           <form>
-            
+            <label htmlFor="goal">New Goal</label>
+            <input id="goal" type="text" name="goal" />
+            <div onClick={handleClick.bind(this)} className="dropdown">
+              <button className="dropbtn">Dropdown</button>            
+              <div className="dropdown-content">
+                <a>Daily</a>
+                <a>Core</a>
+                <a>Short-term</a>
+                <a>Long-term</a>
+              </div>
+            </div>
           </form>
           <div className="columns">
             <div>
